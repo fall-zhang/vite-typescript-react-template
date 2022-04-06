ea8cf45d3f6b98572783ef83465ec017d7a61258
DESKTOP-T6MD988\19852 <a90172451@gmail.com>
2022-04-06 21:58:27 +0800

    更新后可以运行该项目

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 /**
  * 当前环境变量
  */
-export const whyEnv = import.meta.env.VITE_REACT_URL || ''
+console.log(import.meta)
+// export const whyEnv = process.env.NODE_ENV.VITE_REACT_URL || ''
 /**
  * 接口地址
  * @description env 可为主要环境或自定义地址
@@ -19,4 +20,4 @@ export const proxyApi = '/api'
  * 生产环境不需要代理，同时本地配置的代理在生产环境也是不能用的
  */
 // console.log(process.env.NODE_ENV)
-export const urlPrefix = import.meta.env.NODE_ENV === 'development' ? proxyApi : ''
+export const urlPrefix = process.env.NODE_ENV === 'development' ? proxyApi : ''
