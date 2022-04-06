6739f80df0ad89c796048bfa1108634a3893d507
DESKTOP-T6MD988\19852 <a90172451@gmail.com>
2022-04-06 22:12:52 +0800

    更新 eslint

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,20 @@
 /**
  * 当前环境变量
  */
-console.log(import.meta)
+// 浏览器中使用的是 import.meta
+// console.log(import.meta)
+// console.log(process.env)
 // export const whyEnv = process.env.NODE_ENV.VITE_REACT_URL || ''
 /**
  * 接口地址
  * @description env 可为主要环境或自定义地址
  */
-export const apiAddress = 'http://localhost:3030/'
+export const apiAddress = "http://localhost:3030/";
 
 /**
- * 开发代理前缀
+ * 开发环境下，代理前缀
  */
-export const proxyApi = '/api'
+export const proxyApi = "/api";
 
 /**
  * 接口前缀
@@ -20,4 +22,4 @@ export const proxyApi = '/api'
  * 生产环境不需要代理，同时本地配置的代理在生产环境也是不能用的
  */
 // console.log(process.env.NODE_ENV)
-export const urlPrefix = process.env.NODE_ENV === 'development' ? proxyApi : ''
+export const urlPrefix = process.env.NODE_ENV === "development" ? proxyApi : "";
