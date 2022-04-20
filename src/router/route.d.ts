118ccdaae9ca1a15533883d7f6e1c65523c41784
DESKTOP-T6MD988\19852 <a90172451@gmail.com>
2022-04-20 15:50:05 +0800

    配置路由，配置的路由只有两层

--- src/router/route.d.ts
+++ src/router/route.d.ts
@@ -5,11 +5,11 @@ export interface RouteMeta {
   hidden?: boolean
   auth?: string
 }
-// 单条路由
+// 单条路由，路由下面最多嵌套一层菜单
 export interface RouteParam {
   exact?: boolean
   path: string
   component: LazyExoticComponent<ComponentType<unknown>>|React.FC
   meta: RouteMeta
-  children?:RouteParam[]
+  children?:Omit<RouteParam,'children'>[]
 }
--- src/router/router.config.ts
+++
@@ -1,6 +0,0 @@
-import constantRoute from './routes/ConstantRoutes'
-import variableRoute from './routes/VariableRoutes'
-import type { RouteParam } from './route'
-const allRoutes: RouteParam[] = constantRoute.concat(variableRoute)
-
-export default allRoutes
